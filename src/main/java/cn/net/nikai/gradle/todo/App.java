c121ec64ac212bcd12e07523c6d9614d08defbda
nikai1006 <keni1006@163.com>
2018-05-11 01:34:56 +0800

    Add files via upload

---
+++ src/main/java/cn/net/nikai/gradle/todo/App.java
@@ -0,0 +1,15 @@
+package cn.net.nikai.gradle.todo;
+
+/**
+ * todo cn.net.nikai.gradle.todo
+ *
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 1:21 2018/5/11
+ * @Modified By:
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/cn/net/nikai/gradle/todo/AppTest.java
@@ -0,0 +1,20 @@
+package cn.net.nikai.gradle.todo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * todo cn.net.nikai.gradle.todo
+ *
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 1:26 2018/5/11
+ * @Modified By:
+ */
+public class AppTest {
+
+    @Test
+    public void main() {
+    }
+}
